507526957531f21d80cf8019fab93b1ba41ee2db
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-21 13:04:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(sourceArr, callback) {
+    let arr = [];
+    for (let i = 0; i < sourceArr.length; i++) {
+        arr.push(callback(sourceArr[i]));
+    }
+    return arr;
+}
+
+function reduce(sourceArr, callback, startingPoint = 0) {
+  let returnValue = (!!startingPoint) ? startingPoint : sourceArr[0]
+  let i = (!!startingPoint) ? 0 : 1
+  for (; i < sourceArr.length; i++) {
+    returnValue = callback(sourceArr[i], returnValue)
+  }
+  return returnValue
+} 
